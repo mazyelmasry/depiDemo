415da9bbb7e48b72d0227de66c4f284c4d7a0d62
Mazy ElMasry <mazymelmasry@gmail.com>
2024-12-20 16:17:50 +0200

    login

--- app/src/main/java/com/example/depiandstudio/MainActivity.kt
+++ app/src/main/java/com/example/depiandstudio/MainActivity.kt
@@ -10,58 +10,16 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity()
 {
-    lateinit var leftButton:Button
-    lateinit var rightButton:Button
-    lateinit var textView:TextView
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        leftButton = findViewById(R.id.left_button)
-        rightButton = findViewById(R.id.right_button)
-//        textView = findViewById(R.id.textView)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        leftButton.setOnClickListener()
-        {
-            leftButtonClicked()
-        }
+        setContentView(R.layout.login)
 
 
-        rightButton.setOnClickListener()
-        {
 
-            rightButtonClicked()
-        }
 
     }
 
-    fun rightButtonClicked()
-    {
-//        textView.text = "Right button clicked"
-//        rightButton.text = "Clicked"
-//        rightButton.setBackgroundColor(R.color.black)
-//        rightButton.setTextColor(R.color.white)
-//        leftButton.setBackgroundColor(R.color.white)
-//        leftButton.setTextColor(R.color.black)
-        leftButton.text = "Left Button"
-    }
-    fun leftButtonClicked()
-    {
-//        textView.text = "Left button clicked"
-//        leftButton.text = "Clicked"
-//        leftButton.setBackgroundColor(R.color.black)
-//        leftButton.setTextColor(R.color.white)
-//        rightButton.setBackgroundColor(R.color.white)
-//        rightButton.setTextColor(R.color.black)
-        rightButton.text = "Right Button"
-    }
 }
